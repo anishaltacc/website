60a5cfec69334ddcfe76a396751bfbe2836b3a72
Anish Sharma <anish@Anishs-MacBook-Pro.local>
2026-01-05 21:00:08 +0545

    second version

---
+++ products-data.js
@@ -0,0 +1,52 @@
+// Product data for shop
+const products = [
+    {
+        name: "21st Century Schizoid Man",
+        price: 1000,
+        description: "12 by 12 inches (30.5 by 30.5 cm) • Textured high quality art paper • Includes matching stickers and bookmarks",
+        category: "Art Print",
+        image: "https://images.unsplash.com/photo-1549887534-1541e9326642?w=800&q=80"
+    },
+    {
+        name: "Absente",
+        price: 350,
+        description: "5.8 by 6.4 inches (16.26 by 14.73 cm) • Textured high quality art paper",
+        category: "Art Print",
+        image: "https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?w=800&q=80"
+    },
+    {
+        name: "Aditi",
+        price: 1000,
+        description: "12 by 12 inches (30.5 by 30.5 cm) • Textured high quality art paper",
+        category: "Art Print",
+        image: "https://images.unsplash.com/photo-1541961017774-22349e4a1262?w=800&q=80"
+    },
+    {
+        name: "Damn Croissants!",
+        price: 1000,
+        description: "12 by 12 inches (30.5 by 30.5 cm) • Textured high quality art paper",
+        category: "Art Print",
+        image: "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=800&q=80"
+    },
+    {
+        name: "Here Comes the Sun!",
+        price: 1000,
+        description: "12 by 12 inches (30.5 by 30.5 cm) • Textured high quality art paper",
+        category: "Art Print",
+        image: "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?w=800&q=80"
+    },
+    {
+        name: "Noor",
+        price: 1000,
+        description: "12 by 12 inches (30.5 by 30.5 cm) • Textured high quality art paper",
+        category: "Art Print",
+        image: "https://images.unsplash.com/photo-1518709268805-4e9042af9f23?w=800&q=80"
+    },
+    {
+        name: "Sticker Pack",
+        price: 120,
+        description: "1 set includes 4 different artwork stickers",
+        category: "Stickers",
+        image: "https://images.unsplash.com/photo-1626785774573-4b799315345d?w=800&q=80"
+    }
+];
